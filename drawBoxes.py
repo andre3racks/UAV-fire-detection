ba50b5da42d4d64ceb3cceaae0a705c9e11e2e86
Nicholas Roethel <nicholasroethel@Nicholass-MacBook-Air.local>
2020-06-28 15:11:57 -0700

    first commit

---
+++ drawBoxes.py
@@ -0,0 +1,9 @@
+# Based off of the work done by Mark Jay: https://youtu.be/Fwcbov4AzQo
+
+import os
+import matplotlib.pyplot as plt
+import cv2
+from matplotlib.widgets import RectangleSelector
+from generate_xml import write_xml
+
+if __name__ == '__main__':
