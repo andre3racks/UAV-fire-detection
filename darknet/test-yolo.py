284e63d4026603edd4bce3d1349f711cb61de3da
Nicholas Roethel <nicholasroethel@Nicholass-MacBook-Air.local>
2020-07-28 23:54:12 -0700

    added another video

--- darknet/test-yolo.py
+++ darknet/test-yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae44affe5cdb12131f2c552d34d86ebcb0d14decb98b1565f1bb607912225e38
-size 1632
+oid sha256:c05d6d03c33ae22a49d9775d4e922c2b9dded06fed1102a57d3c0e080da10e2d
+size 1633
